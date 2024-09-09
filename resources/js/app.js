38d56f79e8f6ec8ea7909c8d121b577d423fe6ce
anhdinh <anhdinh0408@gmail.com>
2024-09-09 15:43:15 +0700

    Add all project files

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+import toastr from 'toastr';
+import 'toastr/build/toastr.min.css';  // Import CSS của toastr
+
+
+window.Alpine = Alpine;
+
+Alpine.start();
