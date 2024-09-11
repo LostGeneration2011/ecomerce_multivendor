5a0ec0468f43fef98df602ce1df1c633bade8cc4
anhdinh <anhdinh0408@gmail.com>
2024-09-11 17:00:10 +0700

    Update Vendor Profile

--- resources/views/vendor/dashboard/layouts/master.blade.php
+++ resources/views/vendor/dashboard/layouts/master.blade.php
@@ -21,6 +21,9 @@
   <link rel="stylesheet" href="{{ asset('frontend/css/ranger_style.css') }}">
   <link rel="stylesheet" href="{{ asset('frontend/css/jquery.classycountdown.css') }}">
   <link rel="stylesheet" href="{{ asset('frontend/css/venobox.min.css') }}">
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+
+
   <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
   <link rel="stylesheet" href="{{ asset('frontend/css/responsive.css') }}">
 
@@ -64,43 +67,56 @@
   ==============================-->
 
 
- <!--jquery library js-->
-<script src="{{ asset('frontend/js/jquery-3.6.0.min.js') }}"></script>
-<!--bootstrap js-->
-<script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
-<!--font-awesome js-->
-<script src="{{ asset('frontend/js/Font-Awesome.js') }}"></script>
-<!--select2 js-->
-<script src="{{ asset('frontend/js/select2.min.js') }}"></script>
-<!--slick slider js-->
-<script src="{{ asset('frontend/js/slick.min.js') }}"></script>
-<!--simplyCountdown js-->
-<script src="{{ asset('frontend/js/simplyCountdown.js') }}"></script>
-<!--product zoomer js-->
-<script src="{{ asset('frontend/js/jquery.exzoom.js') }}"></script>
-<!--nice-number js-->
-<script src="{{ asset('frontend/js/jquery.nice-number.min.js') }}"></script>
-<!--counter js-->
-<script src="{{ asset('frontend/js/jquery.waypoints.min.js') }}"></script>
-<script src="{{ asset('frontend/js/jquery.countup.min.js') }}"></script>
-<!--add row js-->
-<script src="{{ asset('frontend/js/add_row_custon.js') }}"></script>
-<!--multiple-image-video js-->
-<script src="{{ asset('frontend/js/multiple-image-video.js') }}"></script>
-<!--sticky sidebar js-->
-<script src="{{ asset('frontend/js/sticky_sidebar.js') }}"></script>
-<!--price ranger js-->
-<script src="{{ asset('frontend/js/ranger_jquery-ui.min.js') }}"></script>
-<script src="{{ asset('frontend/js/ranger_slider.js') }}"></script>
-<!--isotope js-->
-<script src="{{ asset('frontend/js/isotope.pkgd.min.js') }}"></script>
-<!--venobox js-->
-<script src="{{ asset('frontend/js/venobox.min.js') }}"></script>
-<!--classycountdown js-->
-<script src="{{ asset('frontend/js/jquery.classycountdown.js') }}"></script>
 
-<!--main/custom js-->
-<script src="{{ asset('frontend/js/main.js') }}"></script>
+ <!--jquery library js-->
+ <script src="{{ asset('frontend/js/jquery-3.6.0.min.js') }}"></script>
+ <!--bootstrap js-->
+ <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
+ <!--font-awesome js-->
+ <script src="{{ asset('frontend/js/Font-Awesome.js') }}"></script>
+ <!--select2 js-->
+ <script src="{{ asset('frontend/js/select2.min.js') }}"></script>
+ <!--slick slider js-->
+ <script src="{{ asset('frontend/js/slick.min.js') }}"></script>
+ <!--simplyCountdown js-->
+ <script src="{{ asset('frontend/js/simplyCountdown.js') }}"></script>
+ <!--product zoomer js-->
+ <script src="{{ asset('frontend/js/jquery.exzoom.js') }}"></script>
+ <!--nice-number js-->
+ <script src="{{ asset('frontend/js/jquery.nice-number.min.js') }}"></script>
+ <!--counter js-->
+ <script src="{{ asset('frontend/js/jquery.waypoints.min.js') }}"></script>
+ <script src="{{ asset('frontend/js/jquery.countup.min.js') }}"></script>
+ <!--add row js-->
+ <script src="{{ asset('frontend/js/add_row_custon.js') }}"></script>
+ <!--multiple-image-video js-->
+ <script src="{{ asset('frontend/js/multiple-image-video.js') }}"></script>
+ <!--sticky sidebar js-->
+ <script src="{{ asset('frontend/js/sticky_sidebar.js') }}"></script>
+ <!--price ranger js-->
+ <script src="{{ asset('frontend/js/ranger_jquery-ui.min.js') }}"></script>
+ <script src="{{ asset('frontend/js/ranger_slider.js') }}"></script>
+ <!--isotope js-->
+ <script src="{{ asset('frontend/js/isotope.pkgd.min.js') }}"></script>
+ <!--venobox js-->
+ <script src="{{ asset('frontend/js/venobox.min.js') }}"></script>
+ <!--classycountdown js-->
+ <script src="{{ asset('frontend/js/jquery.classycountdown.js') }}"></script>
+ <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+ <!--main/custom js-->
+ <script src="{{ asset('frontend/js/main.js') }}"></script>
+
+ <!--Show Dynamic Error-->
+ <script>
+     @if ($errors->any())
+             @foreach($errors->all() as $error)
+                 // Display an info toast with no title
+                     toastr.error("{{$error}}")
+             @endforeach
+
+         @endif
+ </script>
 
 </body>
 
